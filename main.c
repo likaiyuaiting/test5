771df4565155ba2a209139b2fac0363428ed1c27
likaiyuaiting <31584540+likaiyuaiting@users.noreply.github.com>
2017-11-23 16:08:01 +0800

    Update main.c
    在线改变数据方法的学习

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main(void)
 {
+	int i=0;
+	int array[]={2,3,4,5,3,4,8,6};
+	int n=sizeof(array)/array[0];
+	for(i=0;i<n;i++)
+		printf("%d  ",array[i]);
+	printf("\n");
 	printf("haha\n");
 	return 0;
 }
